36d4bb0b23a9c84c0ca1dceefa3813f4332ec0d9
Paweł Kłopotek-Główczewski <pawelkg@pawelkg.com>
2019-08-28 14:33:21 +0200

    Update Url Helper (remove unused methods, fix normalizeUrl method to better handle IDN domains)

--- src/Helpers/Url.php
+++ src/Helpers/Url.php
@@ -3,30 +3,47 @@ declare(strict_types=1);
 
 namespace Wszetko\Sitemap\Helpers;
 
+/**
+ * Class Url
+ *
+ * @package Wszetko\Sitemap\Helpers
+ */
 class Url
 {
-    public static function check(string $url): bool
+    /**
+     * @param string $url
+     *
+     * @return bool|string
+     *
+     * @see https://bugs.php.net/bug.php?id=52923
+     * @see https://www.php.net/manual/en/function.parse-url.php#114817
+     */
+    public static function normalizeUrl(string $url)
     {
-        return (bool)filter_var(self::normalizeUrl($url), FILTER_VALIDATE_URL);
-    }
+        $encodedUrl = preg_replace_callback(
+            '%[^:/@?&=#]+%usD',
+            function ($matches) {
+                return urlencode($matches[0]);
+            },
+            $url
+        );
 
-    public static function normalizeUrl(string $url): string
-    {
-        $url = parse_url($url);
+        $url = parse_url($encodedUrl);
 
         if (!$url || !isset($url['host'])) {
-            return '';
+            return false;
         }
 
+        $url = array_map('urldecode', $url);
         $url['host'] = idn_to_ascii($url['host'], IDNA_DEFAULT, INTL_IDNA_VARIANT_UTS46);
 
-        if (!self::checkDomain($url['host'])) {
-            return '';
+        if (empty($url['scheme']) || !is_string($url['host']) || !self::checkDomain($url['host'])) {
+            return false;
         }
 
         return
             $url['scheme'] . '://'
-            . ((isset($url['user'])) ? $url['user'] . ((isset($url['pass'])) ? ':' . $url['pass'] : '') . '@' : '')
+            . (isset($url['user']) ? $url['user'] . ((isset($url['pass'])) ? ':' . $url['pass'] : '') . '@' : '')
             . $url['host']
             . ((isset($url['port'])) ? ':' . $url['port'] : '')
             . ((isset($url['path'])) ? $url['path'] : '')
@@ -34,8 +51,15 @@ class Url
             . ((isset($url['fragment'])) ? '#' . $url['fragment'] : '');
     }
 
+    /**
+     * @param string $domain
+     *
+     * @return bool
+     */
     public static function checkDomain(string $domain): bool
     {
-        return (bool)filter_var($domain, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME);
+        $domain = idn_to_ascii($domain, IDNA_DEFAULT, INTL_IDNA_VARIANT_UTS46);
+
+        return (bool) filter_var($domain, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME);
     }
 }
