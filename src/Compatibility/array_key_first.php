7fdc8af2c6bc078f993e1b817b3a8dca509003d5
Paweł Kłopotek-Główczewski <pawelkg@pawelkg.com>
2019-09-03 12:57:53 +0200

    Fix for php 7.2

---
+++ src/Compatibility/array_key_first.php
@@ -0,0 +1,11 @@
+<?php
+
+// For compatibility with PHP < 7.3
+if (!function_exists('array_key_first')) {
+    function array_key_first(array $arr) {
+        foreach($arr as $key => $unused) {
+            return $key;
+        }
+        return NULL;
+    }
+}
