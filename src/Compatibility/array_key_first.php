190cd14dbeb1fa0468b6e0e27de0c94f358717eb
Paweł Kłopotek-Główczewski <pawelkg@pawelkg.com>
2019-09-03 12:58:47 +0200

    StyleCI

--- src/Compatibility/array_key_first.php
+++ src/Compatibility/array_key_first.php
@@ -2,10 +2,11 @@
 
 // For compatibility with PHP < 7.3
 if (!function_exists('array_key_first')) {
-    function array_key_first(array $arr) {
-        foreach($arr as $key => $unused) {
+    function array_key_first(array $arr)
+    {
+        foreach ($arr as $key => $unused) {
             return $key;
         }
-        return NULL;
+        return null;
     }
 }
